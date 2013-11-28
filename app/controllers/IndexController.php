5cddbb2e7b39d921c40c179b8fe390be62d6a943
rakeshe <emailrakesh@hotmail.com>
2013-11-28 12:17:04 +1100

    add Page model to handle data for page

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -4,11 +4,28 @@ class IndexController extends ControllerBase
 {
 
     const DEFAULT_PAGE_CAMPAIGN = 'test-campaign';
+    protected $baseUri;
+    protected $languageCode;
+    protected $campaignName;
+    protected $menuTabMain;
+    protected $menuTabSub;
+
+    /** @var  Users */
+    protected $user;
+
+    /** @var Menu */
+    protected $menu;
+
+    /** @var  translation */
+    protected $translation;
+    protected $data;
+
     public function initialize()
     {
         $this->view->setTemplateAfter('main');
         Phalcon\Tag::setTitle('Welcome');
         parent::initialize();
+        $this->setupPage();
     }
 
     public function indexAction()
@@ -22,31 +39,82 @@ class IndexController extends ControllerBase
 
     public function pageAction()
     {
-        $language = $this->dispatcher->getParam("language");
-        $campaignName = null == $this->dispatcher->getParam("campaignName") ? self::DEFAULT_PAGE_CAMPAIGN : $this->dispatcher->getParam("campaignName");
-        $menuTabMain  = $this->dispatcher->getParam("menuTabMain");
-        $menuTabSub   = $this->dispatcher->getParam("menuTabSub");
-        $translation = new translation($language);
-        $menu = new Menu();
-        $currentUser = ['name' => 'Joe Bloggs',
-        'email' => 'joe.bloggs@nowhere.com',
-        'loyaltyTier' => 'Gold',
-        'rewardPoints' => 120];
+
         $this->view->setVars(
             array(
-                'language'     => $language,
-                'campaignName' => $campaignName,
-                'menuTabMain'  => $menuTabMain,
-                'menuTabSub'   => $menuTabSub,
-                'menuItemsTop' => $menu->getMenuItem('top'),
-                'menuItemsSite' => $menu->getMenuItem('site'),
-                'menuItemsLanguageOptions' => $menu->getMenuItem('languageOptions'),
-                'menuItemsAccount' => $menu->getMenuItem('account'),
-                'currentUser' => $currentUser,
-                "t" => $translation->getMessages(),
+                'baseUri'                  => $this->baseUri,
+                'languageCode'             => $this->languageCode,
+                'campaignName'             => $this->campaignName,
+                'menuTabMain'              => $this->menuTabMain,
+                'menuTabSub'               => $this->menuTabSub,
+                'menuItemsTop'             => $this->menu->getMenuItem('top'),
+                'menuItemsSite'            => $this->menu->getMenuItem('site'),
+                'menuItemsLanguageOptions' => $this->menu->getMenuItem('languageOptions'),
+                'menuItemsAccount'         => $this->menu->getMenuItem('account'),
+                'currentUser'              => $this->user->getCurrentUser() ,
+                "t"                        => $this->translation->getMessages(),
+                "pageData"      => $this->data
             )
         );
 
+
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLanguageCode()
+    {
+        return $this->languageCode;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMenuTabMain()
+    {
+        return $this->menuTabMain;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMenuTabSub()
+    {
+        return $this->menuTabSub;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCampaignName()
+    {
+        return $this->campaignName;
+    }
+
+    protected function setupPage()
+    {
+        $this->user = new Users();
+        $this->menu = new Menu();
+
+        $this->languageCode = $this->dispatcher->getParam("languageCode");
+        $this->campaignName = null == $this->dispatcher->getParam(
+            "campaignName"
+        ) ? self::DEFAULT_PAGE_CAMPAIGN : $this->dispatcher->getParam("campaignName");
+        $this->menuTabMain  = $this->dispatcher->getParam("menuTabMain");
+        $this->menuTabSub   = $this->dispatcher->getParam("menuTabSub");
+        $this->translation  = new translation($this->languageCode);
+        $this->baseUri = '/' . $this->languageCode . '/' . $this->campaignName;
+
+        // Setup data for the page
+        $dataModel = new Page();
+        $dataModel
+            ->setLanguageCode($this->languageCode)
+            ->setCampaignName($this->campaignName)
+            ->setMenuTabMain($this->menuTabMain)
+            ->setMenuTabSub($this->menuTabSub);
+
+        $this->data = $dataModel->getData();
 
     }
 }
--- app/models/Page.php
+++ app/models/Page.php
@@ -6,3 +6,86 @@
  * @since      27/11/13 5:17 PM
  * @version    1.0
  */
+class Page extends Phalcon\Mvc\Model
+{
+
+    protected $dataFilePath;
+    protected $data;
+    protected $languageCode = 'en';
+    protected $campaignName;
+    protected $menuTabMain;
+    protected $menuTabSub;
+
+    public function getData(){
+        if(null === $this->data){
+            $this->data = $this->loadDataFile();
+        }
+        return $this->data;
+    }
+
+    protected function loadDataFile(){
+        $data = [];
+        $this->dataFilePath = __DIR__.'/../../cache/campaign_data_' . $this->languageCode .'.php';
+        if(file_exists($this->dataFilePath)){
+            require $this->dataFilePath;
+            return $data;
+
+        }
+    }
+    /**
+     * @param mixed $languageCode
+     */
+    public function setLanguageCode($languageCode)
+    {
+        $this->languageCode = $languageCode;
+        return $this;
+    }
+
+    /**
+     * @param mixed $campaignName
+     */
+    public function setCampaignName($campaignName)
+    {
+        $this->campaignName = $campaignName;
+        return $this;
+    }
+
+    /**
+     * @param mixed $menuTabMain
+     */
+    public function setMenuTabMain($menuTabMain)
+    {
+        $this->menuTabMain = $menuTabMain;
+        return $this;
+    }
+
+    /**
+     * @param mixed $menuTabSub
+     */
+    public function setMenuTabSub($menuTabSub)
+    {
+        $this->menuTabSub = $menuTabSub;
+        return $this;
+    }
+
+
+    public function getConnectionService()
+    {
+
+    }
+
+    public function setForceExists($bool = true)
+    {
+
+    }
+
+    public function dumpResult($var, $foo)
+    {
+
+    }
+
+    public function getConnection()
+    {
+
+    }
+}
