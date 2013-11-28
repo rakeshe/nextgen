ed554393f904a95dd917ec5b87b3ea0bfbc44e25
rakeshe <emailrakesh@hotmail.com>
2013-11-28 12:15:54 +1100

    dynamic menu and template

--- app/library/Menu.php
+++ app/library/Menu.php
@@ -9,39 +9,6 @@
 class Menu extends Phalcon\Mvc\User\Component
 {
     protected $menuItems;
-    public static $menuItemsDefault = [
-        'top'             => [
-            'menu_my_bookings'      => "https://www.hotelclub.com/trips/current",
-            'menu_my_club'          => "https://www.hotelclub.com/account/myclub",
-            'menu_my_account'       => "https://www.hotelclub.com/account/myprofile",
-            'menu_write_a_review'   => "https://www.hotelclub.com/trips/writeReview",
-            'menu_customer_service' => "http://faq.hotelclub.com"
-        ],
-        'languageOptions' => [
-            "en" => "English",
-            "cs" => "繁體中文 (台灣)",
-            "cn" => "简体中文",
-            "de" => "Deutsch",
-            "es" => "Español",
-            "fr" => "Français",
-            "hk" => "繁體中文 (香港)",
-            "it" => "Italiano",
-            "ja" => "日本語",
-            "ko" => "한국어",
-            "ms" => "Bahasa Melayu",
-            "nl" => "Nederlands",
-            "pt" => "Português",
-            "ru" => "Русский",
-            "se" => "Svenska",
-            "th" => "ไทย ",
-        ],
-        'site'            => [
-            'menu_home'          => 'http://www.hotelclub.com/',
-            'menu_club_benefits' => 'http://www.hotelclub.com/club-benefits'
-
-        ],
-
-    ];
 
     public function __construct()
     {
@@ -57,14 +24,4 @@ class Menu extends Phalcon\Mvc\User\Component
         }
     }
 
-    /*public function getTopMenu(){
-        $return = '';
-        foreach($this->menuItems['top'] as $label => $uri){
-            $return .= '<li><a rel="nofollow" class="link" href="'. $uri.'">' . $t->_('menu_my_bookings') }}",';
-        }
-
-    }
-    protected function buildMenu(){
-
-    }*/
 }
