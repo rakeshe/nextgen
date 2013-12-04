1f3cd6eb22d5cea2413989e57d4d20ac9a5951d9
rakeshe <emailrakesh@hotmail.com>
2013-12-04 15:12:59 +1100

    simple couch test routine

--- public/test.php
+++ public/test.php
@@ -6,3 +6,23 @@
  * @since      4/12/13 8:31 AM
  * @version    1.0
  */
+// Config Settings
+define("COUCHBASE_HOSTS", "127.0.0.1");
+define("COUCHBASE_BUCKET", "beer-sample");
+define("COUCHBASE_PASSWORD", "");
+define("COUCHBASE_CONN_PERSIST", true);
+define("INDEX_DISPLAY_LIMIT", 20);
+
+// adjust these parameters to match your installation
+$cb = new Couchbase(COUCHBASE_HOSTS, "beer-sample", COUCHBASE_PASSWORD, COUCHBASE_BUCKET, COUCHBASE_CONN_PERSIST);
+
+$doc = $cb->get(
+    "21st_amendment_brewery_cafe"
+);
+
+if ($doc) {
+    // Decode the JSON string into a PHP array
+    $doc     = json_decode($doc, true);
+    print_r($doc);
+
+}
