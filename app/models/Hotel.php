d327f259dc8d0a8c359f1241c94d57a9fa8a4229
rakeshe <emailrakesh@hotmail.com>
2013-12-04 15:12:05 +1100

    add hotel model

--- app/models/Hotel.php
+++ app/models/Hotel.php
@@ -6,6 +6,45 @@
  * @version    1.0
  */
  
-class Hotel {
+class Hotel extends Phalcon\Mvc\Model
+{
+    protected $id;
+    protected $name;
+    protected $location;
+    protected $stars;
+    protected $img = [];
+    protected $href;
+    protected $country;
+    protected $city;
+    protected $promoOffer;
+    protected $memberOffer;
+    protected $conditons;
+    protected $discountPercent;
+    protected $discountAmount;
+    protected $promoCode;
+    protected $promoAmount;
+
+
+
+    /** Abstract implementation required for this class */
+    public function getConnectionService()
+    {
+
+    }
+
+    public function setForceExists($bool = true)
+    {
+
+    }
+
+    public function dumpResult($var, $foo)
+    {
+
+    }
+
+    public function getConnection()
+    {
+
+    }
 
 }
