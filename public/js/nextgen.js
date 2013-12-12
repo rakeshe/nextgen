55b9a6899d4548e4f4fbac820ac943c6b04b43cc
Hariees  RaviKumar <ravikumarp@zenithsoft.com>
2013-12-12 04:57:51 -0800

    Issues fixed in Region Tabs of Tablets and Mobile

--- public/js/nextgen.js
+++ public/js/nextgen.js
@@ -6,7 +6,7 @@
  */
 $(document).ready(function(){
 
-    /** Search Form date picker */
+    /** Search Form Dektop date picker  */
     var closeText = "Close";
     var currentText = "Today";
     var checkRates = "Check Rates";
@@ -36,6 +36,71 @@ $(document).ready(function(){
         showButtonPanel: true,
         onSelect: function(dateText, inst){ $('#checkin').datepicker("option", "maxDate", $('#checkout').val()); }
     });
+ /** /Search Form Tablet date picker */
+
+     /** Search Form Dektop date picker  */
+    var closeText = "Close";
+    var currentText = "Today";
+    var checkRates = "Check Rates";
+    $('#tab_checkin').datepicker({
+        inline: true,
+        dateFormat: 'dd/mm/yy',
+        maxDate: '+364D',
+        minDate: 0,
+        numberOfMonths: 2,
+        showCurrentAtPos: 0,
+        closeText: closeText,
+        currentText: currentText,
+        showButtonPanel: true,
+        firstDay: 0,
+        dayNamesMin: [ "S", "M", "T", "W", "T", "F", "S" ],
+        onSelect: function(dateText, inst){ $('#tab_checkout').datepicker("option", "minDate", $('#tab_checkin').val()); }
+    });
+    $('#tab_checkout').datepicker({
+        inline: true,
+        dateFormat: 'dd/mm/yy',
+        maxDate: '+364D',
+        minDate: 0,
+        numberOfMonths: 2,
+        showCurrentAtPos: 0,
+        closeText: closeText,
+        currentText: currentText,
+        showButtonPanel: true,
+        onSelect: function(dateText, inst){ $('#tab_checkin').datepicker("option", "maxDate", $('#tab_checkout').val()); }
+    });
+ /** /Search Form Tablet date picker */
+
+     /** Search Form Mobile date picker  */
+    var closeText = "Close";
+    var currentText = "Today";
+    var checkRates = "Check Rates";
+    $('#mob_checkin').datepicker({
+        inline: true,
+        dateFormat: 'dd/mm/yy',
+        maxDate: '+364D',
+        minDate: 0,
+        numberOfMonths: 2,
+        showCurrentAtPos: 0,
+        closeText: closeText,
+        currentText: currentText,
+        showButtonPanel: true,
+        firstDay: 0,
+        dayNamesMin: [ "S", "M", "T", "W", "T", "F", "S" ],
+        onSelect: function(dateText, inst){ $('#mob_checkout').datepicker("option", "minDate", $('#mob_checkin').val()); }
+    });
+    $('#mob_checkout').datepicker({
+        inline: true,
+        dateFormat: 'dd/mm/yy',
+        maxDate: '+364D',
+        minDate: 0,
+        numberOfMonths: 2,
+        showCurrentAtPos: 0,
+        closeText: closeText,
+        currentText: currentText,
+        showButtonPanel: true,
+        onSelect: function(dateText, inst){ $('#mob_checkin').datepicker("option", "maxDate", $('#mob_checkout').val()); }
+    });
+ /** /Search Form Mobile date picker */
 
     /** Carousel controls **/
         $('.carousel').carousel({
@@ -49,23 +114,23 @@ $(document).ready(function(){
 /* Region Tabs-Mobile Toggle Event */
 $(document).ready(function(){
 $( "#regions" ).click(function() {
-$( ".region_panels" ).hide();
-$( ".region_menu" ).show();
+$( "#mbl_banner .region_panels" ).hide();
+$( "#mbl_banner .region_menu" ).show();
 });
 
-$( ".dropdown-toggle" ).click(function() {
+$( ".region_menu .dropdown-toggle" ).click(function() {
 $(this).find("span").toggleClass( "glyphicon-minus");
 if($( ".glyphicon" ).hasClass( "glyphicon-minus" ) == true){
 $(".btn-default").find("span").removeClass("glyphicon-minus");
 $(this).find("span").toggleClass( "glyphicon-minus");
 }
-var button_count=$( "#mbl_banner .region_menu .btn-group-vertical" ).find("button").length; 
+var button_count=$( ".region_menu .btn-group-vertical" ).find("button").length; 
 var get_id=$(this).attr("id").replace(/[^0-9]/g, ''); 
 get_id=get_id-1;
 var setpos = get_id*41;
 setpos=-setpos;
 var getpos= setpos+"px";
-$( "#mbl_banner .region_menu .dropdown-menu" ).css("top",""+getpos+"");
+$( ".region_menu .dropdown-menu" ).css("top",""+getpos+"");
 
 });
 
