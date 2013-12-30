a2d4a52ef141a18e7dcb341a8c6ef5aa4127584d
rakeshe <emailrakesh@hotmail.com>
2013-12-30 17:24:39 +1100

    application cache

--- app/plugins/cache.php
+++ app/plugins/cache.php
@@ -6,3 +6,43 @@
  * @since      4/12/13 7:46 PM
  * @version    1.0
  */
+use Phalcon\Cache\Frontend\Data as DataFrontend,
+    Phalcon\Cache\Multiple,
+    Phalcon\Cache\Backend\Apc as ApcCache,
+    Phalcon\Cache\Backend\Memcache as MemcacheCache,
+    Phalcon\Cache\Backend\File as FileCache;
+class cache extends DataFrontend
+{
+    const LIFETIME = 604800;
+    protected $cache;
+
+    public function __construct(){
+    $this->cache = //Backends are registered from the fastest to the slower
+    $cache = new Multiple(array(
+        new ApcCache($ultraFastFrontend, array(
+            "prefix" => 'cache',
+        )),
+        new MemcacheCache($fastFrontend, array(
+            "prefix" => 'cache',
+            "host" => "localhost",
+            "port" => "11211"
+        )),
+        new FileCache($slowFrontend, array(
+            "prefix" => 'cache',
+            "cacheDir" => "../app/cache/"
+        ))
+    ));
+    }
+}
+/*$ultraFastFrontend = new DataFrontend(array(
+    "lifetime" => 3600
+));
+
+$fastFrontend = new DataFrontend(array(
+    "lifetime" => 86400
+));
+
+$slowFrontend = new DataFrontend(array(
+    "lifetime" => 604800
+));*/
+
---
+++ public/deal.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ *
+ * @package    deal.php
+ * @author     Rakesh Shrestha
+ * @since      12/12/13 8:28 PM
+ * @version    1.0
+ */
