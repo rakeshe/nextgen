c94003a0515fd34c9d8d84b9db8fa746fde6d3ee
Ravii Vadivelou <ravikumarp@machine5591.blr-zenithsoft.net>
2014-11-25 21:17:30 +0530

    Latest Popup works

--- public/themes/geo-map/js/ng.js
+++ public/themes/geo-map/js/ng.js
@@ -1637,3 +1637,83 @@ function displayRegionName(){
 	$(".banner_default_map").show();
 	//$('.text_on_map').css('display') == 'block';
 }//displayRegionName
+
+$(document).ready(function() {
+fn_load();
+/* Date-picker in search form */
+//$('select[name="hotel.rooms[0].chlds"]').val()
+$( "select[name='hotel.rooms[0].chlds']" ).change(function() {
+fn_load();
+});
+function fn_Adob() {
+var  sel;    
+sel = $("select[name='hotel.rooms[0].chlds']" ).val();
+
+if(sel == undefined) sel = 1;
+if (sel == "0") {
+$('.childTravelers').addClass("noneBlock");
+$('#ChildLabel1').removeClass("inlineBlock").addClass("noneInlineBlock"); 
+$('#ChildLabel2').removeClass("inlineBlock").addClass("noneInlineBlock");       
+$('#ChildLabel3').removeClass("inlineBlock").addClass("noneInlineBlock");   
+$('#ChildLabel4').removeClass("inlineBlock").addClass("noneInlineBlock");   
+$('#ChildLabel5').removeClass("inlineBlock").addClass("noneInlineBlock");  
+}
+if (sel == "1") {
+$('.childTravelers').removeClass("noneBlock");
+$('#ChildLabel1').removeClass("noneInlineBlock").addClass("inlineBlock");  
+$('#ChildLabel2').removeClass("inlineBlock").addClass("noneInlineBlock");       
+$('#ChildLabel3').removeClass("inlineBlock").addClass("noneInlineBlock");   
+$('#ChildLabel4').removeClass("inlineBlock").addClass("noneInlineBlock");   
+$('#ChildLabel5').removeClass("inlineBlock").addClass("noneInlineBlock");           
+}
+if (sel == "2") {
+$('.childTravelers').removeClass("noneBlock");
+$('#ChildLabel1').removeClass("noneInlineBlock").addClass("inlineBlock");   
+$('#ChildLabel2').removeClass("noneInlineBlock").addClass("inlineBlock");  
+$('#ChildLabel3').removeClass("inlineBlock").addClass("noneInlineBlock");   
+$('#ChildLabel4').removeClass("inlineBlock").addClass("noneInlineBlock");   
+$('#ChildLabel5').removeClass("inlineBlock").addClass("noneInlineBlock");           
+}
+if (sel == "3") {
+$('.childTravelers').removeClass("noneBlock"); 
+$('#ChildLabel1').removeClass("noneInlineBlock").addClass("inlineBlock");       
+$('#ChildLabel2').removeClass("noneInlineBlock").addClass("inlineBlock");    
+$('#ChildLabel3').removeClass("noneInlineBlock").addClass("inlineBlock");  
+$('#ChildLabel4').removeClass("inlineBlock").addClass("noneInlineBlock");   
+$('#ChildLabel5').removeClass("inlineBlock").addClass("noneInlineBlock");   
+}
+if (sel == "4") {
+$('.childTravelers').removeClass("noneBlock");
+$('#ChildLabel1').removeClass("noneInlineBlock").addClass("inlineBlock");
+$('#ChildLabel2').removeClass("noneInlineBlock").addClass("inlineBlock");         
+$('#ChildLabel3').removeClass("noneInlineBlock").addClass("inlineBlock");  
+$('#ChildLabel4').removeClass("noneInlineBlock").addClass("inlineBlock");  
+$('#ChildLabel5').removeClass("inlineBlock").addClass("noneInlineBlock");   
+}
+if (sel == "5") {
+$('.childTravelers').removeClass("noneBlock");
+$('#ChildLabel1').removeClass("noneInlineBlock").addClass("inlineBlock"); 
+$('#ChildLabel2').removeClass("noneInlineBlock").addClass("inlineBlock");      
+$('#ChildLabel3').removeClass("noneInlineBlock").addClass("inlineBlock");  
+$('#ChildLabel4').removeClass("noneInlineBlock").addClass("inlineBlock");  
+$('#ChildLabel5').removeClass("noneInlineBlock").addClass("inlineBlock");  
+}
+}
+
+
+function fn_load()
+{
+$('.childTravelers').addClass("noneBlock");
+$('#ChildLabel1').removeClass("inlineBlock").addClass("noneInlineBlock"); 
+$('#ChildLabel2').removeClass("inlineBlock").addClass("noneInlineBlock");       
+$('#ChildLabel3').removeClass("inlineBlock").addClass("noneInlineBlock");   
+$('#ChildLabel4').removeClass("inlineBlock").addClass("noneInlineBlock");   
+$('#ChildLabel5').removeClass("inlineBlock").addClass("noneInlineBlock"); 
+
+fn_Adob(); 
+
+}
+ $('.addRoom').on('click', function () {
+    $('<p>Text</p>').appendTo('.hotelGuests ');
+  });
+ });
