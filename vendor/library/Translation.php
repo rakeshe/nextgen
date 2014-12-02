eeaccdf327a8027ea4faf75d8a377b77f3c991ec
rakeshe <Rakesh.Shrestha@hotelclub.com>
2014-12-02 13:26:12 +1100

    trans fro Show more deals

--- vendor/library/Translation.php
+++ vendor/library/Translation.php
@@ -32,24 +32,27 @@ class Translation
         ));
     }
 
-    public  function getCardTranslation()
+    public function getCardTranslation()
     {
-        return json_encode([
-            'book'                  => $this->getTranslation()->offsetGet('book'),
-            'close'                 => $this->getTranslation()->offsetGet('close'),
-            'check_rates'                 => $this->getTranslation()->offsetGet('check_rates'),
-            'day_names'             => explode(',',$this->getTranslation()->offsetGet('day_names')),
-            'day_names_min'         => explode(',',$this->getTranslation()->offsetGet('day_names_min')),
-            'month_names'           => $this->getTranslation()->offsetGet('month_names'),
-            'promo_pc_off_template' => $this->getTranslation()->offsetGet('promo_pc_off_template'),
-            'search'                => $this->getTranslation()->offsetGet('search'),
-            'select'                => $this->getTranslation()->offsetGet('select'),
-            'today'                 => $this->getTranslation()->offsetGet('today'),
-            'mem_extras'            => $this->getTranslation()->offsetGet('mem_extras'),
-            'mem_inactive_line1'    => $this->getTranslation()->offsetGet('mem_inactive_line1'),
-            'mem_inactive_line2'    => $this->getTranslation()->offsetGet('mem_inactive_line2'),
-            'save'                  => $this->getTranslation()->offsetGet('save'),
-        ]);
+        return json_encode(
+            [
+                'book'                  => $this->getTranslation()->offsetGet('book'),
+                'close'                 => $this->getTranslation()->offsetGet('close'),
+                'check_rates'           => $this->getTranslation()->offsetGet('check_rates'),
+                'day_names'             => explode(',', $this->getTranslation()->offsetGet('day_names')),
+                'day_names_min'         => explode(',', $this->getTranslation()->offsetGet('day_names_min')),
+                'month_names'           => $this->getTranslation()->offsetGet('month_names'),
+                'promo_pc_off_template' => $this->getTranslation()->offsetGet('promo_pc_off_template'),
+                'search'                => $this->getTranslation()->offsetGet('search'),
+                'select'                => $this->getTranslation()->offsetGet('select'),
+                'today'                 => $this->getTranslation()->offsetGet('today'),
+                'mem_extras'            => $this->getTranslation()->offsetGet('mem_extras'),
+                'mem_inactive_line1'    => $this->getTranslation()->offsetGet('mem_inactive_line1'),
+                'mem_inactive_line2'    => $this->getTranslation()->offsetGet('mem_inactive_line2'),
+                'save'                  => $this->getTranslation()->offsetGet('save'),
+                'show-more-deals'       => 'Show more deals'
+            ]
+        );
 
     }
     
