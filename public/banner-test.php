2d38dc9d51586a18bfe0bbe2155394ea48ce84d9
rakeshe <Rakesh.Shrestha@hotelclub.com>
2014-12-02 12:49:54 +1100

    more banner flow test page

--- public/banner-test.php
+++ public/banner-test.php
@@ -7,18 +7,9 @@
  * @version    1.0
  */
 ?>
-<html><head><title>Exported ads</title></head><body>
-<b>English (UK) 960 x 83 px</b><br/>
+<html><head><title>Banner Flow Test</title></head><body>
+<b>English (UK) 530 x 300 px</b><br/>
 Flash/Html5 ad:<br/>
-<iframe src="//html5.host.bannerflow.com/ad_631424_178.html?clicktag=" style="border:0;width:960px;height:83px;" scrolling="no" frameborder="0"></iframe>
-<br />
-<br />
-Image ad: (iframe)<br/>
-<iframe src="//html5.host.bannerflow.com/ad_631424_178.html?image=true&clicktag=" style="border:0;width:960px;height:83px;" scrolling="no" frameborder="0"></iframe>
-<br />
-<br />
-Image:<br/>
-<img src="//html5-images.host.bannerflow.com/adhtml_img_631424.gif" />
-<br/><br/>
+<iframe src="//html5.host.bannerflow.com/ad_642802_178.html?clicktag=" style="border:0;width:530px;height:300px;" scrolling="no" frameborder="0"></iframe>
 </body>
 </html>
