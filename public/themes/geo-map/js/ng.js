6fe5f77a76e278fa858b8464860f9aa558a92474
Santosh Hegde <santoshk@zenithsoft.com>
2014-12-03 10:58:59 +0530

    add coupon code in book pop-up

--- public/themes/geo-map/js/ng.js
+++ public/themes/geo-map/js/ng.js
@@ -68,27 +68,27 @@ function log(message) {
 				+ "&hotel.chkout="
 				+ checkOut
 				+ "&search=Search&locale="
-				+ languageCode
+				+ local
 				+ "&lpid.category=hot-mkt-dated&lpid.priority=1200.0&lpid=hotelGpSearch";
 	});
 	$(".hc_find").click(function() {
-				if ($(this).data('code') == 'all') {
+		if ($(this).data('code') == 'all') {
 			checkIn  = $('#choseDatesStartDate_hc').val();
 			checkOut = $('#choseDatesEndDate_hc').val();
 			oneg 	 = $('#choseDatesOneg_hc').val();
-			promo 	 = $('#proCode').val();
+			promo 	 = $('#pp-promo').val();
 		}
-		window.location = "http://www.hotelclub.com/shop/hotelsearch?type=hotel&hsv.showDetails=true&hotel.couponCode="
+		window.location = "http://www.hotelclub.com/shop/hotelsearch?type=hotel&hotel.couponCode="
 				+ promo
+                + "&locale="
+                + local
 				+ "&hotel.hid="
 				+ oneg
 				+ "&hotel.rooms[0].adlts=2&hotel.type=keyword&hotel.chkin="
 				+ checkIn
 				+ "&hotel.chkout="
 				+ checkOut
-				+ "&search=Search&locale=";
-				//+ languageCode
-				//+ "&lpid.category=hot-mkt-dated&lpid.priority=1200.0&lpid=hotelGpSearch";
+				+ "&search=Search";
 	});
 
 /*
