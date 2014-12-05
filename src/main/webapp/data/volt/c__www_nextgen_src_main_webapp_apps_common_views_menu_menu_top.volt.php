3501253307f2f339554f0ab7d9f255825cd85244
rakeshe <Rakesh.Shrestha@hotelclub.com>
2014-12-05 23:58:22 +1100

    release 1.8.3

--- src/main/webapp/data/volt/c__www_nextgen_src_main_webapp_apps_common_views_menu_menu_top.volt.php
+++
@@ -1,5 +0,0 @@
-<?php if (!empty($menuItemsTop)) { ?>
-    <?php foreach ($menuItemsTop as $label => $uri) { ?>
-        <li><a rel="nofollow" class="link" href="<?php echo $uri; ?>"><?php echo $t->_($label); ?></a></li>
-    <?php } ?>
-<?php } ?>
