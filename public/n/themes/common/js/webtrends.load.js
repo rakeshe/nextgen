ea6d0d2c0cda8dce2d85bcb332358c8cce31dd8b
rakeshe <Rakesh.Shrestha@hotelclub.com>
2014-12-07 07:18:16 +1100

    new updated webtrends tracking pixel

---
+++ public/n/themes/common/js/webtrends.load.js
@@ -0,0 +1,20 @@
+// WebTrends SmartSource Data Collector Tag v10.4.1
+// Copyright (c) 2014 Webtrends Inc.  All rights reserved.
+// Tag Builder Version: 4.1.3.2
+// Created: 2014.03.27
+window.webtrendsAsyncInit=function(){
+    var dcs=new Webtrends.dcs().init({
+        dcsid:"dcscfchfzvz5bdrpz13vsgjna_9r8u",
+        domain:"statse.webtrendslive.com",
+        timezone:10,
+        i18n:true,
+        fpcdom:".ctix8.cheaptickets.com",
+        plugins:{
+            //hm:{src:"//s.webtrends.com/js/webtrends.hm.js"}
+        }
+        }).track();
+};
+(function(){
+    var s=document.createElement("script"); s.async=true; s.src="/n/themes/common/js/webtrends.min.js";    
+    var s2=document.getElementsByTagName("script")[0]; s2.parentNode.insertBefore(s,s2);
+}());
