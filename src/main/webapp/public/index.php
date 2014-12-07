9af0e754d9fbb6cbbabf7239fd6f555804c08a46
rakeshe <Rakesh.Shrestha@hotelclub.com>
2014-12-08 04:32:50 +1100

    set default timezone to chicago

--- src/main/webapp/public/index.php
+++ src/main/webapp/public/index.php
@@ -7,7 +7,7 @@
  * @version    1.0
  */
 try {
-
+    date_default_timezone_set('America/Chicago');
 	/**
 	 * The FactoryDefault Dependency Injector automatically register the right services providing a full stack framework
 	 */
--- src/main/webapp/public/ping.php
+++ src/main/webapp/public/ping.php
@@ -1,1 +1,3 @@
-<?php $today = date('Y-m-d h:i:s'); echo 'ping @ ' . $today;?>
+<?php
+date_default_timezone_set('America/Chicago');
+$today = date('Y-m-d h:i:s'); echo 'ping @ ' . $today;?>
