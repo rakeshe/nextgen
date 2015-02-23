326031bd14faf1b5f00661c7744a733cced5487b
rakeshe <Rakesh.Shrestha@hotelclub.com>
2015-02-24 10:00:50 +1100

    register HC\Common as apps

--- src/main/webapp/config/app.config.php
+++ src/main/webapp/config/app.config.php
@@ -1,6 +1,14 @@
 <?php
 
 return array(
+    'api' => array(
+        'className' => 'HC\Api\Module',
+        'path' => '../apps/api/Module.php'
+    ),
+    'common' => array(
+        'className' => 'HC\Common\Module',
+        'path' => '../apps/common/Module.php'
+    ),
     'ti' => array(
         'className' => 'HC\TI\Module',
         'path' => '../apps/ti/Module.php'
@@ -9,10 +17,7 @@ return array(
         'className' => 'HC\Merch\Module',
         'path' => '../apps/merch/Module.php'
     ),
-    'api' => array(
-        'className' => 'HC\Api\Module',
-        'path' => '../apps/api/Module.php'
-    ),
+
     'hk-calendar' => array(
         'className' => 'HC\HkCalendar\Module',
         'path' => '../apps/hk-calendar/Module.php'
