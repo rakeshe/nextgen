f486a08645e26d87a5a015c93420f1d1305cfb65
mohan-zsl <mohanr@zenithsoft.com>
2015-05-20 13:01:43 +0530

    Outside popup-modal click closed the popup

--- src/main/webapp/public/n/themes/deals/js/deals.js
+++ src/main/webapp/public/n/themes/deals/js/deals.js
@@ -1,5 +1,5 @@
+var popUpVal = 0;//set default value for popUpVal to display the popup block
 (function( $, HB ) {
-
     var Deals = {
 
         init : function() {
@@ -48,7 +48,7 @@
         displayRobot : function() {
             var template = HB.compile( $("#robot-template").html() );
             $('body').append(template());
-			$('.modal-wrapper').show();
+			popUpVal = 0;//set popUpVal to display the popup block
         },
 
         displayHeader : function() {
@@ -226,11 +226,26 @@
         });
 
     });
-
+	/*on click '.cancel-function' class close the popup */
 	$(document).on('click','.cancel-action',function(){
-		$('.modal-wrapper').hide();
         Deals.setDropDownDefaultOption().dropWhereDo();
+		$('.modal-wrapper').hide();
+		popUpVal=2;
 	});
-    
+	/*check popup event is visible or not */
+	function popupEvent(){
+		if(popUpVal==1){
+			$('.modal-wrapper').hide();
+			popUpVal=2;
+		}
+		else if(popUpVal==0){
+			$('.modal-wrapper').show();
+			popUpVal=1;
+		}
+		return false;
+	}//popupEvent
+	/*trigger even on outside click*/
+	$(document).click(function(e) {
+		popupEvent();//to check the popupevent 
+	});
 })(jQuery, Handlebars);
-
