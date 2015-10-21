5b629eda4b2bcf0be9560e744b65f052f9f024a3
mohan-zsl <mohanr@zenithsoft.com>
2015-10-21 16:16:09 +0530

    Validation for hcfa registration is completed

--- src/main/webapp/public/n/themes/hcfa/js/app.js
+++ src/main/webapp/public/n/themes/hcfa/js/app.js
@@ -1,5 +1,63 @@
 (function(){
+	$('#btn-register').click(function(e){
+        var travelAgencyName = $("#TA_NAME").val();
+        var name = $("#FNAME").val();
+        var travelAgentId = $("#TA_ID").val();
+        var email = $("#EMAIL").val();
+        var telephone = $("#telephone").val();
 
+		$('#error-message-val').empty();//empty the php error
+		$('.error-message').empty();//empty the js error
+		$('input').removeClass("error");
+		if(travelAgencyName == ''){
+			$('#TA_NAME').addClass("error");
+			$('.error-message').append('Travel agency name is required');
+			$('#TA_NAME').focus();
+			return false;
+		}
+		else if(name == ''){
+			$('#FNAME').addClass("error");
+			$('.error-message').append('Name is required');
+			$('#FNAME').focus();
+			return false;
+		}
+		else if(travelAgentId == ''){
+			$('#TA_ID').addClass("error");
+			$('.error-message').append('Travel agency id is required');
+			$('#TA_ID').focus();
+			return false;
+		}
+		else if(email == ''){
+			$('#EMAIL').addClass("error");
+			$('.error-message').append('Email is required');
+			$('#EMAIL').focus();
+			return false
+		}
+		else if(telephone == ''){
+			$('#telephone').addClass("error");
+			$('.error-message').append('Phone number is required');
+			$('#telephone').focus();
+			return false;
+		}
+
+		//validate email
+		if(!validateEmail(email)){
+			$('#EMAIL').addClass("error");
+			$('.error-message').append('E-mail is not valid');
+			$('#EMAIL').focus();
+			return false;
+		}
+
+		//validate phone
+		if(!validatePhone(telephone)){
+			$('#telephone').val('');
+			$('#telephone').addClass("error");
+			$('.error-message').append('Phone number is not valid');
+			$('#telephone').focus();
+			return false;
+		}
+		return true;
+	});
     $('#activeModel').click(function(e){
         e.preventDefault();
         var docHeight = $(document).height();
@@ -29,3 +87,25 @@
         $("#overlay").remove();
     });
 })();
+
+//validating email
+function validateEmail(email) {
+	var filter = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
+	if (filter.test(email)) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}//validateEmail
+
+//validating phone
+function validatePhone(phone) {
+	var filter = /^[0-9-+]+$/;
+    if (filter.test(phone)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}//validatePhone
