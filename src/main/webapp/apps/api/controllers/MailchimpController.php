33ccfd4017cd6c060d8da24413cd815f294bd38a
Santosh Hegde <santoshk@zenithsoft.com>
2015-10-22 17:34:24 +0530

    Add mailchimp lib through composer

---
+++ src/main/webapp/apps/api/controllers/MailchimpController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ *
+ * @package    MailChimpController.php
+ * @author     K N Santosh Hegde
+ * @since      22/10/2015
+ * @version    1.0
+ */
+
+namespace HC\Api\Controllers;
+
+use Phalcon\Http\Response,
+    Phalcon\Validation\Message;
+
+class MailchimpController extends ControllerBase {
+
+    public function initialize() {
+
+    }
+
+    public function indexAction() {
+        die('yes');
+    }
+
+}
