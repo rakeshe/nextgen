97d417a57ba768fcd8dd34f4119dc669ecf8617c
rakeshe <Rakesh.Shrestha@hotelclub.com>
2015-10-28 10:31:05 +1100

    add multiple ip retrieval methods

--- src/main/webapp/apps/deals/models/DealsModel.php
+++ src/main/webapp/apps/deals/models/DealsModel.php
@@ -452,6 +452,7 @@ class DealsModel extends \Phalcon\Mvc\Model
      */
     public function getClientIp()
     {
+        if(null === $this->clientIp) $this->setClientIp();
         return $this->clientIp;
     }
 
@@ -535,5 +536,31 @@ class DealsModel extends \Phalcon\Mvc\Model
             if ($ip >= 4294967040) return false;
         }
         return true;
+    }
+
+    /**
+     * Another get ip function
+     * @return mixed
+     */
+    public function getUserIP()
+    {
+        $client  = @$_SERVER['HTTP_CLIENT_IP'];
+        $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+        $remote  = $_SERVER['REMOTE_ADDR'];
+
+        if(filter_var($client, FILTER_VALIDATE_IP))
+        {
+            $ip = $client;
+        }
+        elseif(filter_var($forward, FILTER_VALIDATE_IP))
+        {
+            $ip = $forward;
+        }
+        else
+        {
+            $ip = $remote;
+        }
+
+        return $ip;
     }
 }
